a84e63c34c309c96c09e9b7a2829e090d92a306a
damionbrown <dbrown0591>
2017-10-17 15:07:56 -0500

    Finished while.JS

--- js/while.js
+++ js/while.js
@@ -1,19 +1,29 @@
-// var i =2;
-//
-// while (i<=65536){
-//     console.log(i);
-//     i*=2;
-// }
+var i =2;
+
+while (i<=65536){
+    console.log(i);
+    i*=2;
+}
 //
 //
 // // This is how you get a random number between 50 and 100
-// var allCones = Math.floor(Math.random() * 50) + 50;
-// // This expression will generate a random number between 1 and 5
-// Math.floor(Math.random() * 5) + 1;
+var allCones = Math.floor(Math.random() * 50) + 50;
 //
-// var i = 10;
-//
-// do {
-//     console.log('while loop iteration #' + i);
-//     i++;
-// } while (i < 10);
+// This expression will generate a random number between 1 and 5
+
+do {
+
+    var conesOrdered = Math.floor(Math.random() * 5) + 1;
+
+        if (conesOrdered > allCones) {
+            console.log("I can't sell you more than I have.");
+        }
+        else {
+            console.log("I sold " + conesOrdered + " cones.");
+            allCones = allCones - conesOrdered;
+        }
+
+
+
+}while(allCones > 0);
+
